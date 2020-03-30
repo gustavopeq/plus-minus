8939353881431b8f669a524cb5038e5438c60461
gustavopeq <gustavo_pequeno2@hotmail.com>
2020-03-30 10:49:38 -0700

    First commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+	/*
+	 * Given an array of integers, calculate the fractions of its elements that are positive, negative, and are zeros. 
+	 * Print the decimal value of each fraction on a new line.
+	 */
+	
+	public static void main(String[] args) 
+	{
+		
+	}
+
+}
